58f4d708809e21cf868a8ea647026b21d88927c7
Sergey Vichik <sergv@berkeley.edu>
2012-02-08 01:56:28 +0000

    Time ratio for input block

--- MoveEqualVar.m
+++ MoveEqualVar.m
@@ -1,5 +1,9 @@
 function AAs = MoveEqualVar(AAs,orig,copy)
 
 for i=1:length(AAs)
-    AAs{i}(:,orig) = AAs{i}(:,orig) + AAs{i}(:,copy);
+    tmp = AAs{i}(:,copy(1));
+    for j=2:length(copy)
+        tmp = tmp + AAs{i}(:,copy(j));
+    end
+    AAs{i}(:,orig) = AAs{i}(:,orig) + tmp;
 end
