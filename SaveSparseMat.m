75a47fc9d223fe6b217d9c4349803bf1c9499501
Sergey Vichik <sergv@berkeley.edu>
2012-03-17 00:28:10 +0000

    new file

---
+++ SaveSparseMat.m
@@ -0,0 +1,14 @@
+function SaveSparseMat(data,fname)
+[i,j,val] = find(data);
+if (size(i,1) == 1)
+    data_dump = [i;j;val];
+else
+    data_dump = [i,j,val]';
+end
+if (data(end,end) == 0) % no last element
+    data_dump = [data_dump [size(data,1); size(data,2); 0]];
+end
+
+f = fopen(fname,'wt');
+fprintf(f,'%d %d %16.5f\n',data_dump);
+fclose(f);
