a1f367f7b0bdbd0eb886c57fd76163791bd73549
Jason Kong <jasonjkong@gmail.com>
2012-07-16 18:39:44 +0000

    added BLOM2_0_ExtractModel

---
+++ Core/BLOM2_0_ExtractModel.m
@@ -0,0 +1,52 @@
+%> @file BLOM2_0_ExtractModel.m
+%> @brief BLOM 2.0 Outline based on what we discussed previously
+
+%> Overview of BLOM Extract Model Structure
+%> - Find all Input, External, Constraint and Cost BLOM library blocks.
+%> - Build cost and constraint dependency wire graph, starting from Constraint and Cost blocks and traverse backwards.
+%>     -  BFS to find the graph.
+%>     -  Recognize reaching non-BLOM inputs (user error).
+%>     -  Format of the graph: Sparse connectivity matrix of all wires, 
+%>         store just the block handles.
+%>     -  Each wire has a property whether optimization variables is
+%>             introduced for it for each derivative evaluation, 
+%>         intermediate major time step, initial time, final time.
+%>     -  Store for each wire, if it is constrained, cost (discrete or continuous), 
+%>         input, external.
+%> - Flatten the wires:
+%>     -  "Fetch" all PolyBlocks data: variables from the base workspace 
+%>         or values from the dialog (just do evalin('base',get_param(h,'A')).
+%>     -  Get all possible dimensions from the known PolyBlocks (with or without the Simulink 
+%> "compile").
+%>     -  Translate all supported Simulink blocks to PolyBlocks using the known dimensions.   
+%>     -  Eliminate duplicate routing variables. 
+%>     -  Combine all PolyBlocks to a large P and K matrices.
+%>     -  Eliminate whatever is possible (constants, identity PolyBlocks, linearly dependent constraints, replicated P rows, unused terms).
+%>     -  Propagate time property information using the processed P and K structure.    
+%> - Create the whole problem:
+%>     -  Introduce and duplicate all major time step variables, including their dependencies.
+%>     -  Introduce and duplicate minor time steps variables and their dependencies.
+%>     -  Link by the appropriate Butcher tableau values major and minor time steps variables.
+%>     -  Add move blocking constraints.
+%>     -  Eliminate whatever is possible.
+%>     -  Mark external, input.
+%>     -  Take care of cost.
+
+%> @section
+
+%======================================================================
+%> @brief This function extracts a Simulink model and is the first step
+%into converting it into an optimization problem
+%>
+%> More detailed description of the problem.
+%>
+%> @param arg1 First argument
+%> @param arg2 Second argument
+%>
+%> @retval out1 return value for the first output variable
+%> @retval out2 return value for the second output variable
+%======================================================================
+
+function [out1,out2] = BLOM2_0_ExtractModel(arg1,arg2)
+
+end
