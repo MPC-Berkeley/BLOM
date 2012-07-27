97370feda56fca5d1039f139feb64a48071f775f
Jason Kong <jasonjkong@gmail.com>
2012-07-27 00:08:50 +0000

    changed all num2str to sprintf

--- Callbacks/BLOM_ChangeNumberOfPorts.m
+++ Callbacks/BLOM_ChangeNumberOfPorts.m
@@ -21,27 +21,27 @@ function BLOM_ChangeNumberOfPorts(block,inputNum,outputNum)
         muxSource = [block '/Mux'];
         if inputNum > currentNumInputs
             % make mux larger and have proper number of inports
-            set_param(muxSource,'Inputs',num2str(inputNum));
+            set_param(muxSource,'Inputs',sprintf('%d',inputNum));
             set_param(muxSource,'Position',[80 38 85 38+20*inputNum]);
             for i = (currentNumInputs+1):inputNum
                 % create new number of needed inports
-                block_name = [block '/In' num2str(i)];
+                block_name = [block '/In' sprintf('%d',i)];
                 add_block('simulink/Sources/In1',block_name);
                 % set position of current inport block
                 set_param(block_name,'Position',[20 38+(i-1)*30 50 52+(i-1)*30]);
                 % connect to proper mux port
-                add_line(block,['In' num2str(i) '/1'],['Mux/' num2str(i)])
+                add_line(block,['In' sprintf('%d',i) '/1'],['Mux/' sprintf('%d',i)])
             end
         elseif inputNum < currentNumInputs
             for i = currentNumInputs:-1:(inputNum+1)
                 % delete unnecessary lines
-                delete_line(block,['In' num2str(i) '/1'],['Mux/' num2str(i)])
+                delete_line(block,['In' sprintf('%d',i) '/1'],['Mux/' sprintf('%d',i)])
                 % delete unnecessary inports
-                currentBlock = [block '/In' num2str(i)];
+                currentBlock = [block '/In' sprintf('%d',i)];
                 delete_block(currentBlock);
             end
             % resize mux and change inputs
-            set_param(muxSource,'Inputs',num2str(inputNum));
+            set_param(muxSource,'Inputs',sprintf('%d',inputNum));
             set_param(muxSource,'Position',[80 38 85 38+20*inputNum]);
         else
             % do nothing. Number of inports equals number of desired
@@ -54,27 +54,27 @@ function BLOM_ChangeNumberOfPorts(block,inputNum,outputNum)
         demuxSource = [block '/Demux'];
         if outputNum > currentNumOutputs
             % make demux larger and have proper number of outports
-            set_param(demuxSource,'Outputs',num2str(outputNum));
+            set_param(demuxSource,'Outputs',sprintf('%d',outputNum));
             set_param(demuxSource,'Position',[280 38 285 38+20*outputNum]);
             for i = (currentNumOutputs+1):outputNum
                 % create new number of needed outports
-                block_name = [block '/Out' num2str(i)];
+                block_name = [block '/Out' sprintf('%d',i)];
                 add_block('simulink/Sinks/Out1',block_name);
                 % set position of current outport block
                 set_param(block_name,'Position',[320 38+(i-1)*30 350 52+(i-1)*30]);
                 % connect to proper demux port
-                add_line(block,['Demux/' num2str(i)],['Out' num2str(i) '/1'])
+                add_line(block,['Demux/' sprintf('%d',i)],['Out' sprintf('%d',i) '/1'])
             end
         elseif outputNum < currentNumOutputs
             for i = currentNumOutputs:-1:(outputNum+1)
                 % delete unnecessary lines
-                delete_line(block,['Demux/' num2str(i)],['Out' num2str(i) '/1']);
+                delete_line(block,['Demux/' sprintf('%d',i)],['Out' sprintf('%d',i) '/1']);
                 % delete unnecessary outports
-                currentBlock = [block '/Out' num2str(i)];
+                currentBlock = [block '/Out' sprintf('%d',i)];
                 delete_block(currentBlock);
             end
             % resize demux and change outputs
-            set_param(demuxSource,'Outputs',num2str(outputNum));
+            set_param(demuxSource,'Outputs',sprintf('%d',outputNum));
             set_param(demuxSource,'Position',[280 38 285 38+20*outputNum]);
         else
             % do nothing. Number of outports equals number of desired
