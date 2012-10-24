1991a33b7ec7184c44eaa38ab0255714155aa6b2
Tony Kelman <tony@kelman.net>
2012-10-24 00:46:47 +0000

    This is a useful preprocessing step that we haven't touched in a while - a permutation matrix accomplishes the same functionality roughly 2 orders of magnitude faster

--- Matrix/BLOM_Filter_poly.m
+++ Matrix/BLOM_Filter_poly.m
@@ -1,11 +1,18 @@
 function [Aunique Cunique] = BLOM_Filter_poly(A,C)
 
 [Aunique, I, J] = unique(A,'rows');
+perm = sparse(1:length(J), J, 1, length(J), size(Aunique,1));
+Cunique = C*perm;
+%{
 idxs = 1:size(Aunique,1);
 sources = idxs(J);
-Cunique = zeros(size(C,1),size(Aunique,1));
+Cunique_old = zeros(size(C,1),size(Aunique,1));
 for j=1:size(C,1)
     for i=idxs
-        Cunique(j,i) = sum(C(j,sources == i));
+        Cunique_old(j,i) = sum(C(j,sources == i));
     end
 end
+if ~isequal(Cunique,Cunique_old)
+    error('mismatch')
+end
+%}
