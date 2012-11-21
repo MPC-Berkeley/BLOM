d2df86ddb8c877e454d8083d457895ae0edc91fa
Jason Kong <jasonjkong@gmail.com>
2012-11-21 18:29:37 +0000

    Added comments to let user know which blocks should be found when going through testBFS

--- Examples/testscript.m
+++ Examples/testscript.m
@@ -4,3 +4,13 @@ simin = [0 1];
 profile on;
 a = BLOM_ExtractModel('testBFS',1,1,1,1);
 profile viewer;
+
+%% note, should find the following blocks in the BFS search
+
+% Add1, Integrator, Add, InputFromWorkSpace, InputFromSimulink, Polyblock2,
+% ExternalFromWorkSpace, ExternalFromSimulink, Mux?, Add3, Add2, Constant2, Constant3,
+% Unit Delay1, Polyblock5, Polyblock3, From, UnitDelay, Polyblock,
+% Subsystem, (Inside Subsystem: Unit Delay2, subadd1, product1, Constant8,
+% Constant5), Subsystem1, (Inside Subsystem1: sub_Polyblock1, sub_Product1,
+% sub_Constant4, sub_Constant5), Constant8
+% TOTAL: 31 blocks to find
