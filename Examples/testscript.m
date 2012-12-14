81bdfe0cbf9def3e1e24cec95642b781c3124cef
Jason Kong <jasonjkong@gmail.com>
2012-12-14 19:54:05 +0000

    added code to help debug where the BFS may have problems

--- Examples/testscript.m
+++ Examples/testscript.m
@@ -2,16 +2,48 @@
 simin = [0 1];
 
 profile on;
-a = BLOM_ExtractModel('testBFS',1,1,1,1);
+[a,block,allVars] = BLOM_ExtractModel('testBFS',1,1,1,1);
 profile viewer;
 
 %% note, should find the following blocks in the BFS search (parentheses
-%% has the number of outports that each block should have if it has more than one)
+%% has the number of outports that each block should have if it has more
+%% than one)
 
-% Add1, Integrator, Add, InputFromWorkSpace, InputFromSimulink, Polyblock2 (2),
-% ExternalFromWorkSpace, ExternalFromSimulink, Mux?, Add3, Add2, Constant2, Constant3,
-% Unit Delay1, Polyblock5, Polyblock3, From, UnitDelay, Polyblock(2),
-% Subsystem, (Inside Subsystem: Unit Delay2, subadd1, product1, Constant8,
-% Constant5), Subsystem1 (2), (Inside Subsystem1: sub_Polyblock1, sub_Product1,
-% sub_Constant4, sub_Constant5), Constant8
-% TOTAL: 31 blocks to find
+testBFS_Blocks = {'Add1','Integrator','Add','InputFromWorkspace','InputFromSimulink',...
+    'Polyblock2','ExternalFromWorkspace','ExternalFromSimulink1','Add3','Add2','Constant2','Constant3',...
+    'Unit Delay1','Polyblock5','From','Unit Delay','Polyblock','From1','Demux','Product',...
+    'Polyblock3','Subsystem','Subsystem/Unit Delay2','Subsystem/subadd1',...
+    'Subsystem/Product1','Subsystem/Constant8','Subsystem/Constant5','From2','From3',...
+    'Subsystem1','Subsystem1/sub_Polyblock1',...
+    'Subsystem1/sub_Product1','Subsystem1/sub_Constant4','Subsystem1/sub_Constant5','Constant8'};
+
+for i = 1:length(testBFS_Blocks)
+    testBFS_Blocks{i} = ['testBFS/' testBFS_Blocks{i}];
+end
+    
+% for testBFS, make sure that the proper blocks were found from the BFS
+
+fprintf('-----------------------------------\n')
+fprintf('-----------------------------------\n')
+
+fprintf('Blocks incorrectly found:\n')
+for i = 1:length(block.names)
+    % test to see if it found incorrect blocks
+    if any(strcmp(testBFS_Blocks,block.names{i}))
+        % do nothing, it found the block
+    else
+        block.names{i}
+    end
+end
+
+fprintf('\n\n-----------------------------------\n')
+fprintf('-----------------------------------\n')
+fprintf('Blocks that were not found:\n')
+for i = 1:length(testBFS_Blocks)
+    % test to see if it found all the correct blocks
+    if any(strcmp(testBFS_Blocks{i},block.names))
+        % do nothing, it found the block
+    else
+        testBFS_Blocks{i}
+    end
+end
