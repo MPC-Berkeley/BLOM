0f2825cfff9a785bad6f358e1e273ea0719f1c1f
Eric Bourgain-Chang <ebc@berkeley.edu>
2013-04-24 19:16:37 +0000

    Now calls testfun for each model.

--- Examples/TestTimeRelevance/testallscript.m
+++ Examples/TestTimeRelevance/testallscript.m
@@ -1,14 +1,2 @@
-% test script
-simin = [0 1];
-
-% testManyBound test script
-horizon = 4;
-[a,blockMB,stepVarsMB,allVarsMB] = BLOM_ExtractModel('testManyBound',horizon,1,1,1);
-
-% testManyDelay test script
-horizon=4;
-[a,blockMD,stepVarsMD,allVarsMD] = BLOM_ExtractModel('testManyDelay',horizon,1,1,1);
-
-% testSubsystem test script
-horizon=4;
-[a,blockS,stepVarsS,allVarsS] = BLOM_ExtractModel('testSubsystem',horizon,1,1,1);
+% test each model
+testfun('testManyBound')
