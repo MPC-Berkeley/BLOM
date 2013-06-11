a1d1d738954b5546df8f680bd496f46951cb2aff
Kyle Chiang <kylechiang@berkeley.edu>
2013-06-11 21:09:45 +0000

    Our Version of HelloWorld

---
+++ Examples/HelloWorld/RunHelloWorldR2.m
@@ -0,0 +1,26 @@
+clear
+
+%% Convert model to optimization problem
+[ModelSpec,block,stepVars,allVars] = BLOM_ExtractModel('HelloWorldNew',10,1,'Euler',1);
+%%
+[RunResults2 ResultsVec] = BLOM_RunModel(ModelSpec);
+%%
+SolverStruct = BLOM_ExportToSolver(ModelSpec,'IPOPT');
+
+[OptGuess ExtVars InitialStates ] = BLOM_SplitResults(ModelSpec,RunResults);
+
+SolverStructData =  BLOM_SetProblemData(SolverStruct,ModelSpec,OptGuess, ExtVars, InitialStates);
+
+SolverResult  =  BLOM_RunSolver(SolverStructData,ModelSpec);
+
+
+%% plot the results
+subplot(211);
+plot(SolverResult.System_state);
+
+ylabel('X');
+
+subplot(212);
+plot(SolverResult.System_u);
+ylabel('U');
+
