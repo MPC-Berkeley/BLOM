f0ad8902a83066dcf578a966f76b6de43b4f386b
Kyle Chiang <kylechiang@berkeley.edu>
2013-06-27 18:56:51 +0000

    copy CreateIpoptDAT.m to development/legacy

---
+++ Legacy/CreateIpoptDAT.m
@@ -0,0 +1,15 @@
+function CreateIpoptDAT(name,fixed,x0)
+
+f = fopen([ name 'X0.dat'],'wt');
+fprintf(f,'%.18g \n',x0);
+fclose(f);
+
+f = fopen([ name 'Fixed.dat'],'wt');
+
+%Data = zeros(length(fixed.AAs),1);
+%for i=1:length(fixed.AAs)
+%    Data(i) = fixed.Cs{i}(2);
+%end
+
+fprintf(f,'%.18g \n',fixed);
+fclose(f);
